c478d83b9e24cecc748c7d54282862d742c38ee2
Carlos <carlos.albmr@usp.br>
2025-06-08 21:04:03 -0300

    Melhorado o design da home

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Home.css'; // Importa o CSS
+import './Home.css';
+import GamePlanLogo from '../../assets/images/GamePlanLogo.png';
 
 function Home() {
   const navigate = useNavigate();
@@ -14,10 +15,15 @@ function Home() {
 
   return (
     <div className="home-container">
-      <h1>Bem-vindo ao GamePlan</h1>
-      <h2>Em qual modo você quer acessar?</h2>
-      <button onClick={goToHome_Usuario}>Modo usuário</button>
-      <button onClick={goToLogin}>Modo treinador</button>
+      <div className="home-box">
+        <img src={GamePlanLogo} alt="GamePlan Logo" className="logo" />
+        <h1>Bem-vindo ao GamePlan!</h1>
+        <h2>Em qual modo você deseja acessar?</h2>
+        <div className="button-group">
+          <button onClick={goToHome_Usuario}>Modo usuário</button>
+          <button onClick={goToLogin}>Modo treinador</button>
+        </div>
+      </div>
     </div>
   );
 }
